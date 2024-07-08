09f9b91a34221f0c8543ccc7492ec0f6e1d89d1d
Moyi <johan01elgrande@hotmail.es>
2024-07-08 02:30:58 -0400

    Made all functions just one big function

--- Reels Downloader.py
+++ Reels Downloader.py
@@ -1,72 +1,59 @@
 import instaloader
 import yt_dlp
 
-# Function to download Instagram reels.          
-def reels(link):
-
-    # To create an instance of the IG API.
-    ig_api = instaloader.Instaloader()
-
-    # This gets the video's unique identifier.
-    shortcode = link.split("/")[-2]
-
-    # This is for downloading the reel.
-    post = instaloader.Post.from_shortcode(ig_api.context, shortcode)
-    ig_api.download_post(post, target="Reels")
-
-# Function to download TikTok videos.
-def tiktoks(link):
-
-    # Set the options for downloading the best quality video.
-    ydl_opts = {
-    'format': 'bestvideo[height<=1080]+bestaudio/best[height<=1080]',
-    'outtmpl': r'C:\Users\johan\TG_BOT\TikToks\%(title)s.%(ext)s',
-    'merge_output_format': 'mp4'
-    }
-
-    # To download the video.
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([link])
-
-# Function to download Youtube videos.
-def yt_videos(link):
-
-    # Set the options for downloading the best quality video.
-    ydl_opts = {
-
-        'format': 'bestvideo[height<=1980]+bestaudio/best[height<=1080]',
-        'outtmpl': r'C:\Users\johan\tg_bot\Youtube Videos\%(title)s.%(ext)s',
-        'merge_output_format': 'mp4',
-    }
-
-    # To download the video.
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([link])
-
-def general_videos(link):
+def downloader(link):
     
-    # Set the options for downloading the best quality video.
-    ydl_opts = {
+    if  link.find("instagram") == 12:
 
-        'format': 'bestvideo[height<=1980]+bestaudio/best[height<=1080]',
-        'outtmpl': r'C:\Users\johan\tg_bot\General Videos\%(title)s.%(ext)s',
-        'merge_output_format': 'mp4',
-    }
+        # To create an instance of the IG API.
+        ig_api = instaloader.Instaloader()
 
-    # To download the video.
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([link])
+        # This gets the video's unique identifier.
+        shortcode = link.split("/")[-2]
+
+        # This is for downloading the reel.
+        post = instaloader.Post.from_shortcode(ig_api.context, shortcode)
+        ig_api.download_post(post, target="Reels")
+    
+    elif link.find("tiktok") == 12:
+
+        # Set the options for downloading the best quality video.
+        ydl_opts = {
+        'format': 'bestvideo[height<=1080]+bestaudio/best[height<=1080]',
+        'outtmpl': r'C:\Users\johan\TG_BOT\TikToks\%(title)s.%(ext)s',
+        'merge_output_format': 'mp4'
+        }
+
+        # To download the video.
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([link])
+    
+    elif link.find("youtube") == 12:
+        
+        # Set the options for downloading the best quality video.
+        ydl_opts = {
+
+            'format': 'bestvideo[height<=1980]+bestaudio/best[height<=1080]',
+            'outtmpl': r'C:\Users\johan\tg_bot\Youtube Videos\%(title)s.%(ext)s',
+            'merge_output_format': 'mp4',
+        }
+
+        # To download the video.
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([link])
+    else:
+        # Set the options for downloading the best quality video.
+        ydl_opts = {
+
+            'format': 'bestvideo[height<=1980]+bestaudio/best[height<=1080]',
+            'outtmpl': r'C:\Users\johan\tg_bot\General Videos\%(title)s.%(ext)s',
+            'merge_output_format': 'mp4',
+        }
+
+        # To download the video.
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([link])
 
 # Menu to enter a link.
 link = str(input("Introduzca el link del video que desea descargar: "))
-
-if  link.find("instagram") == 12:
-    reels(link)
-
-elif link.find("tiktok") == 12:
-    tiktoks(link)
-
-elif link.find("youtube") == 12:
-    yt_videos(link)
-else:
-    general_videos(link)
+downloader(link)
